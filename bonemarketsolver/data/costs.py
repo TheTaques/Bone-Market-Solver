3fa2e667148a3f8378c35ebb00c5933969aaaac6
TheTaques <35156969+TheTaques@users.noreply.github.com>
2021-08-18 21:05:07 +0200

    Update costs.py

--- bonemarketsolver/data/costs.py
+++ bonemarketsolver/data/costs.py
@@ -9,10 +9,18 @@ class Cost(Enum):
     """The number of pennies needed to produce a quality."""
 
     # This is your baseline EPA: the pennies you could generate using an action for a generic grind.
-    ACTION = 400
+    ACTION = 409
+
+    # Casing...
+    # Using Casing from Parabola, yields 7 CP per Action
+    # see https://fallenlondon.wiki/wiki/Find_the_weakness_in_an_opponent%27s_defences
+    CASING_CP = ACTION/7
 
     # Antique Mystery
-    ANTIQUE_MYSTERY = 1250
+    # Using:
+    # https://fallenlondon.wiki/wiki/Steal_an_Antique_Mystery_from_Feducci
+    # with Parabolan Casing
+    ANTIQUE_MYSTERY = ACTION + 32*CASING_CP
 
     # Favours: Bohemians
     # Various opportunity cards
@@ -22,7 +30,8 @@ class Cost(Enum):
     BONE_FRAGMENT = 1
 
     # Cartographer's Hoard
-    CARTOGRAPHERS_HOARD = 31250
+    # Using https://fallenlondon.wiki/wiki/Money-Making#Creating_Cartographer.27s_Hoards
+    CARTOGRAPHERS_HOARD = 110.035*ACTION
 
     # Favours: The Church
     # Various opportunity cards
@@ -31,6 +40,10 @@ class Cost(Enum):
     # Collection Note: There's a 'Package' in London
     # Station VIII Lab
     COLLECTION_NOTE = ACTION
+
+    # Volume of Collated Research
+    # Using https://fallenlondon.wiki/wiki/Ambush_an_Inattentive_Scholar
+    COLLATED_RESEARCH = ACTION + 21*CASING_CP/3
 
     # Deep-Zee Catch
     # Spear-fishing at the bottom of the Evenlode, 7 at a time
@@ -110,7 +123,7 @@ class Cost(Enum):
     INCISIVE_OBSERVATION = 50
 
     # Crate of Incorruptible Biscuits
-    INCORRUPTIBLE_BISCUITS = 250
+    INCORRUPTIBLE_BISCUITS = 12*HINTERLAND_SCRIP
 
     # Inkling of Identity
     INKLING_OF_IDENTITY = 10
@@ -119,20 +132,16 @@ class Cost(Enum):
     # Feast of the Exceptional Rose, sent by one player and accepted by another
     ENGRAVED_SKULL = 2*ACTION + 200*INKLING_OF_IDENTITY
 
-    # Ivory Humerus
-    # Ealing Gardens statue, 2 at a time
-    IVORY_HUMERUS = (ACTION + 4*BOHEMIAN_FAVOURS)/2
-
     # Jade Fragment
-    JADE_FRAGMENT = 1
+    JADE_FRAGMENT = 2
 
     # Femur of a Jurassic Beast
     # Brawling for yourself, large Bone Market crate, 12 at a time
     JURASSIC_FEMUR = (10*ACTION)/12
 
     # Knotted Humerus
-    # These are accumulated while acquiring other qualities.
-    KNOTTED_HUMERUS = 0
+    # https://fallenlondon.wiki/wiki/Ambush_an_Inattentive_Scholar
+    KNOTTED_HUMERUS = ACTION + 21*CASING_CP/2
 
     # Nevercold Brass Sliver
     NEVERCOLD_BRASS = 1
@@ -142,8 +151,12 @@ class Cost(Enum):
     OBSIDIAN_TAIL = cp_model.INT32_MAX/2
 
     # Parabolan Orange-apple
-    # Parabolan Base-camp, electricity and hedonism, 2 at a time
-    ORANGE_APPLE = (2*ACTION + 100*BONE_FRAGMENT + 21*HEDONIST_CP)/2
+    # Parabolan Base-camp
+    ORANGE_APPLE = 2*ACTION + 1100*BONE_FRAGMENT
+    
+    # Ivory Humerus
+    # Bohemian Sculptress
+    IVORY_HUMERUS = ACTION + ORANGE_APPLE
 
     # Ivory Femur
     # Bohemian Sculptress
@@ -161,7 +174,8 @@ class Cost(Enum):
     PENTAGRAMMIC_SKULL = 9*ACTION
 
     # Hand-picked Peppercaps
-    PEPPERCAPS = HINTERLAND_SCRIP
+    # A Disillusioned Fungiculturalist
+    PEPPERCAPS = ACTION/10
 
     # Revisionist Historical Narrative
     # Waswood
@@ -193,7 +207,7 @@ class Cost(Enum):
 
     # Survey of the Neath's Bones
     # Laboratory Research
-    SURVEY = 6*ACTION/25
+    SURVEY = 8*ACTION/25
 
     # Plaster Tail Bones
     # Carpenter's Granddaughter, 2 at a time
@@ -217,7 +231,8 @@ class Cost(Enum):
 
     # Thorned Ribcage
     # Iron-Toothed Terror Bird
-    THORNED_RIBCAGE = 6*ACTION
+    # At Dangerous 292 and Glasswork 12, this takes 7.42 actions
+    THORNED_RIBCAGE = 7.42*ACTION
 
     # Flourishing Ribcage
     # Helicon House
@@ -251,16 +266,21 @@ class Cost(Enum):
     # Keeper of the Marigold Menagerie
     SKELETON_WITH_SEVEN_NECKS = ACTION + GROUSE_OBSERVATION
 
+    # Nodule of Trembling Amber
+    TREMBLING_AMBER = 1250
+
     # Rubbery Skull
     # Flute Street, including travel due to quality cap
     RUBBERY_SKULL = 25*ACTION
 
     # Rumour of the Upper River
-    RUMOUR_OF_THE_UPPER_RIVER = 250
+    # Travel by Railway is the best option
+    RUMOUR_OF_THE_UPPER_RIVER = ACTION
 
     # Jet Black Stinger
-    # Hunting with Sophia's, 5 at a time
-    BLACK_STINGER = (ACTION + 5*RUMOUR_OF_THE_UPPER_RIVER)/5
+    # "Propose a hunting expedition" at Sophia's, 5 at a time
+    # D200 challenge, for D295 -> 88% chance
+    BLACK_STINGER = (5*RUMOUR_OF_THE_UPPER_RIVER - 15 + 1/.88 * ACTION) / 5
 
     # Prismatic Frame
     # Expedition at Station VIII
@@ -316,5 +336,3 @@ class Cost(Enum):
     # Withered Tentacle
     # Helicon House, 3 at a time
     WITHERED_TENTACLE = (ACTION + 5*WARM_AMBER)/3
-
-
